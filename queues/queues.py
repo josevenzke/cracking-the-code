e226f8c6c4e5a562f6d93e52d1844daabe45c7ad
josevenzke <josevenzkejr@gmail.com>
2022-10-04 11:27:30 -0300

    queue implementation

---
+++ queues/queues.py
@@ -0,0 +1,65 @@
+from dataclasses import dataclass
+
+
+class Node:
+    def __init__(self, data: int,next:object = None) -> None:
+        self.data = data
+        self.next = next
+
+
+class Queue:
+    def __init__(self):
+        self.first = None
+        self.last = None
+    
+    def add(self,val:int):
+        node = Node(val)
+        if not self.first:
+            self.first = node
+        else:
+            self.last.next = node
+        self.last = node
+    
+    def remove(self):
+        if not self.first:
+            raise Exception
+        tmp = self.first
+        self.first = self.first.next
+        if not self.first:
+            self.last = None
+        print('removed -> ',tmp.data)
+        return tmp
+
+    def peek(self):
+        if self.first:
+            print(self.first.data)
+
+    def is_empty(self):
+        print(self.first == None)
+
+x = Queue()
+
+x.add(1)
+x.add(2)
+x.add(3)
+x.add(4)
+x.add(5)
+
+x.peek()
+x.remove()
+
+x.peek()
+x.remove()
+
+x.is_empty()
+
+x.peek()
+x.remove()
+
+x.peek()
+x.remove()
+
+x.peek()
+x.remove()
+
+x.is_empty()
