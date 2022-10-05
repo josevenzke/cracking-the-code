d8157d8ac3761451dd030891ba782eb31ab753e0
josevenzke <josevenzkejr@gmail.com>
2022-10-05 20:56:06 -0300

    exercise 3.4 done

--- stacks/stacks.py
+++ stacks/stacks.py
@@ -68,3 +68,95 @@ def teste():
     x.peek()
     x.pop()
 
+
+#3.3 Stack of Plates: implement a set of stacks
+
+class StackNode:
+    def __init__(self,data: int,next= None):
+        self.data = data
+        self.next = next
+
+class Stack:
+    def __init__(self,capacity=100):
+        self.capacity = capacity
+        self.size = 0 
+        self.top = None
+
+
+    def push(self,data) -> None:
+        node = StackNode(data)
+        node.next = self.top
+        self.top = node
+        self.size+=1
+    
+    def pop(self)-> StackNode:
+        if self.is_empty():
+            raise EmptyStack
+        tmp = self.top
+        self.top = self.top.next
+        self.size -=1
+        return tmp
+    
+    def is_full(self):
+        return self.capacity == self.size
+    
+    def is_empty(self):
+        return self.top == None
+
+class SetOfStacks:
+    def __init__(self,capacity):
+        self.capacity = capacity
+        self.stacks = Stack()
+        self.stacks.push(Stack(self.capacity))
+
+    def push(self,val):
+        stack = self.stacks.top.data
+        if stack.is_full():
+            stack = self._add_stack()
+        stack.push(val)
+
+    def pop(self):
+        stack = self.stacks.top.data
+        stack.pop()
+        if stack.is_empty():
+            stack = self._remove_stack()
+    
+    def peek(self):
+        stack = self.stacks.top.data
+        print(stack.top.data)
+
+    def pop_at(self,index):
+        curr_idx = 0
+        stack = self.stacks.top
+        while stack:
+            if curr_idx == index:
+                return stack.data.pop()
+            stack = stack.next
+            curr_idx+=1
+        print('out of range')
+
+    def _add_stack(self):
+        print('added new stack')
+        self.stacks.push(Stack(self.capacity))
+        return self.stacks.top.data
+    
+    def _remove_stack(self):
+        print('removed empty stack')
+        self.stacks.pop()
+        return self.stacks.top.data
+
+x = SetOfStacks(5)
+
+x.push(1)
+x.push(2)
+x.push(3)
+x.push(4)
+x.peek()
+x.push(5)
+x.peek()
+x.push(6)
+x.push(7)
+x.pop_at(1)
+x.pop()
+x.pop()
+x.peek()
